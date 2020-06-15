0410bbfbccbe5bf9056640bf3919de456803172f
Abtahee Ali <abtaheeali@gmail.com>
2020-06-15 14:16:05 -0400

    Asked user for two numbers

--- public/main.js
+++ public/main.js
@@ -6,3 +6,11 @@ console.log(
 
 const userName = window.prompt('What is your name?')
 console.log(`It is nice to meet you, ${userName}!`)
+
+const valueOne = window.prompt('Please input a number.')
+const valueTwo = window.prompt('Please input another number.')
+
+const firstOperand = parseFloat(valueOne)
+const secondOperand = parseFloat(valueTwo)
+console.log(firstOperand)
+console.log(secondOperand)
