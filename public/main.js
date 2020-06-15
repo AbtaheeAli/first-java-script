a8c84a51166997ddd209df8864b7905b4121c6cc
Abtahee Ali <abtaheeali@gmail.com>
2020-06-15 13:43:25 -0400

    Prompted user for their name and greeted user

--- public/main.js
+++ public/main.js
@@ -3,3 +3,6 @@ const fullName = 'Abtahee Ali'
 console.log(
   `Hi, my name is ${fullName} and I drink ${numberOfCoffee} coffee per day.`
 )
+
+const userName = window.prompt('What is your name?')
+console.log(`It is nice to meet you, ${userName}!`)
