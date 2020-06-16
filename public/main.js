f650935841fa9222d7453ac89d56c2954f141427
Abtahee Ali <abtaheeali@gmail.com>
2020-06-15 20:06:25 -0400

    Fixed spacing

--- public/main.js
+++ public/main.js
@@ -1,10 +1,12 @@
 const numberOfCoffee = 'one'
 const fullName = 'Abtahee Ali'
+
 console.log(
   `Hi, my name is ${fullName} and I drink ${numberOfCoffee} coffee per day.`
 )
 
 const userName = window.prompt('What is your name?')
+
 console.log(`It is nice to meet you, ${userName}!`)
 
 const valueOne = window.prompt('Please input a number.')
